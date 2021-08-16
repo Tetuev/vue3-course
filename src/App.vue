3b74d70ddbc306db5bacbc74c3a83f2938b28ca7
Roman <source@bk.ru>
2021-08-16 14:07:15 +0300

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,69 @@
 <template xmlns="http://www.w3.org/1999/html">
-   <div>
-     <div>
-       <button v-on:click="addLike">Like</button>
-       <button v-on:click="addDislike">DisLike</button>
-     </div>
-     <div>Кол-во лайков: <strong>{{likes}}</strong></div>
-     <div>Кол-во дизлайков: <strong>{{dislikes}}</strong></div>
+   <div class="app">
+       <form>
+         <h4>Создание поста</h4>
+         <input class="input" type="text" placeholder="Название">
+         <input class="input" type="text" placeholder="Описание">
+         <button class="btn">Создать</button>
+       </form>
+        <div class="post" v-for="post in posts">
+          <div><strong>Название:</strong> {{post.title}}</div>
+          <div><strong>Описание:</strong> {{post.body}}</div>
+        </div>
    </div>
-
 </template>
 
 <script>
 export default {
   data(){
     return{
-      likes: 0,
-      dislikes: 5,
+      posts: [
+        {id: 1, title: 'Javascript', body: 'Описание поста'},
+        {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+      ]
     }
   },
   methods:{
-    addLike(){
-     this.likes += 1;
-    },
-    addDislike(){
-      this.dislikes += 1;
-    }
+
+
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app{
+  padding: 20px;
+}
+.post{
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form{
+  display: flex;
+  flex-direction: column;
+}
 
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn{
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
 
-// Single file component
